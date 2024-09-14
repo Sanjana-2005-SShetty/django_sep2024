bb29f2e2f00bd8c74d694a5ffa67079638efa927
Sanjana-2005-SShetty <sanjanasshetty0@gmail.com>
2024-09-14 15:58:20 +0530

    test github

--- myproject/students/urls.py
+++ myproject/students/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.student,name='student'),
+    path('',views.student_list,name='student_list'),
+    path('create/',views.student_create,name='student_create'),
+    path('students/update/<int:id>/',views.student_update,name='student_update'),
+    # path('delete/<int:id>/',views.student_delete,name='student_delete'),
 ]
